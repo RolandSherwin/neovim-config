a4187cf1daf1a621ffba62625f4f6899682bda85
RolandSherwin <RolandSherwin@protonmail.com>
2022-10-26 00:00:33 +0530

    small changes

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -2,8 +2,9 @@ local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
   return
 end
-
-bufferline.setup {
+bufferline.setup()
+--bufferline.setup {
+local gg = {
   options = {
     numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
